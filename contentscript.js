b00728422af96fc3bf7e2fe564394153817f88a2
Kostya Dubinin <kostya.dubinin@gmail.com>
2018-02-07 22:44:56 +0100

    Add Dutch prononciation

---
+++ contentscript.js
@@ -0,0 +1,18 @@
+var target = document.querySelector('#lemmaContent');
+
+var observer = new MutationObserver(function(mutations) {
+  var word = $('.gag').text()
+  var src = "https://protected-dusk-33233.herokuapp.com/?text=" + word
+  $('.gag').after("<a href=# onclick='document.getElementById(&quot;player&quot;).play()'><span class='g2 gb3 gbf'></span></a>")
+  $('.gag').after("<audio id=player><source src=" + src + " type='audio/mp3'>Your browser does not support the audio tag.</audio>")
+  $('.gag').after("<span class='gn'> </span>")
+
+  mutations.forEach(function(mutation) {
+    console.log(mutation.type);
+  });
+});
+
+var config = { attributes: true, childList: true, characterData: true };
+observer.observe(target, config);
+
+// onclick="document.getElementById(&quot;player&quot;).play()"
