802a02e92fd7f7728bad448963730a8206914185
Kostya Dubinin <kostya.dubinin@gmail.com>
2018-02-19 21:12:32 +0100

    Refactor

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,12 @@
-var target = document.querySelector('#lemmaContent');
-
 var observer = new MutationObserver(function(mutations) {
-  var word = $('.gag').text()
-  var src = "https://protected-dusk-33233.herokuapp.com/?text=" + word
-  $('.gag').after("<a href=# onclick='document.getElementById(&quot;player&quot;).play()'><span class='g2 gb3 gbf'></span></a>")
-  $('.gag').after("<audio autoplay id=player><source src=" + src + " type='audio/mp3'>Your browser does not support the audio tag.</audio>")
+  var word = $(".gag").text()
+  var prononciationSrc = "https://protected-dusk-33233.herokuapp.com/?text=" + word
+  var onclick = "document.getElementById('player').play()"
+  $('.gag').after("<a href=# onclick=" + onclick + "><span class='g2 gb3 gbf'></span></a>")
+  $('.gag').after("<audio id=player><source src=" + prononciationSrc + " type='audio/mp3'>Your browser does not support the audio tag.</audio>")
   $('.gag').after("<span class='gn'> </span>")
 });
 
+var target = document.querySelector("#lemmaContent");
 var config = { attributes: true, childList: true, characterData: true };
 observer.observe(target, config);
